3801065e5875ac14ad02b0d6ce220ef887511084
Babita Singh <babitasinghtech@gmail.com>
2025-05-02 01:10:49 +0530

    "Initial Commit"

--- lib/Pages/Chat/Widgets/chatbubble.dart
+++ lib/Pages/Chat/Widgets/chatbubble.dart
@@ -29,25 +29,43 @@ class ChatBubble extends StatelessWidget {
           ),
           decoration: BoxDecoration(
             color: Theme.of(context).colorScheme.primaryContainer,
-            borderRadius: BorderRadius.only(
+            borderRadius: isComing ? BorderRadius.only(
               bottomLeft: Radius.circular(0),
               bottomRight: Radius.circular(10),
               topLeft: Radius.circular(10),
               topRight: Radius.circular(10),
-            ),
+            ): 
+            BorderRadius.only(
+              bottomLeft: Radius.circular(10),
+              bottomRight: Radius.circular(0),
+              topLeft: Radius.circular(10),
+              topRight: Radius.circular(10),
           ),
-          child: Text(message),
+          ),
+          child: imageUrl == ""
+          ? Text(message): Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children:[
+              Image.network(
+                imageUrl
+              ),
+              SizedBox(height: 5),
+              Text(message),
+            ],
+          ) ,
         ),
         SizedBox(height: 3),
            Row(
-              // crossAxisAlignmnet: isComing? CrossAxisAlignment.start: CrosssAxisAlignment.end,
               mainAxisAlignment: isComing? MainAxisAlignment.start: MainAxisAlignment.end,
               children: [
                 isComing
-                    ? Text(time)
+                    ? Text(time,
+                    style:Theme.of(context).textTheme.labelMedium)
                     : Row(
                       children: [
-                        Text(time),
+                        Text(time,
+                        style:Theme.of(context).textTheme.labelMedium),
+                        SizedBox(width: 5),
                         SvgPicture.asset(AssetsImage.chatSeenSVG,
                         width:20),
                       ],
--- lib/Pages/Chat/chatPage.dart
+++ lib/Pages/Chat/chatPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:social_media/Pages/Chat/Widgets/chatbubble.dart';
 import 'package:social_media/conflig/images.dart';
 
@@ -23,14 +24,56 @@ class SingleChatPage extends StatelessWidget {
 
         actions: [
           IconButton(onPressed: () {}, icon: const Icon(Icons.phone)),
-
           IconButton(onPressed: () {}, icon: const Icon(Icons.video_call)),
         ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: Container(
+        margin: EdgeInsets.all(10),
+        padding: EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(100),
+          color: Theme.of(context).colorScheme.primaryContainer,
+        ),
+        child: Row(
+          children: [
+             Container(
+              width: 30,
+              height: 30,
+
+            SvgPicture.asset(AssetsImage.chatMicSVG, width: 25),
+             ),
+            SizedBox(width: 10),
+            Expanded(
+              child: TextField(
+                decoration: InputDecoration(
+                  filled: false,
+                  hintText: "Type a message.......",
+                ),
+              ),
+            ),
+            SizedBox(width: 10),
+
+             Container(
+              width: 30,
+              height: 30,
+
+            SvgPicture.asset(AssetsImage.chatGallerySVG,width: 25),
+             ),
+            SizedBox(width: 10),
+            Container(
+              width: 30,
+              height: 30,
+
+              child: SvgPicture.asset(AssetsImage.chatSendSVG,width: 25),
+            ),
+          ],
+        ),
       ),
 
       body: Padding(
         padding: const EdgeInsets.all(10),
-        child: Column(
+        child: ListView(
           children: [
             ChatBubble(
               message: "Hello Lori How are you?",
@@ -40,11 +83,50 @@ class SingleChatPage extends StatelessWidget {
               time: "10:00 AM",
             ),
             ChatBubble(
-              message: "Hello Lori How are you?",
+              message: "I'm perfactly fine",
               isComing: false,
               imageUrl: "",
               status: "seen",
-              time: "10:00 AM",
+              time: "10:01 AM",
+            ),
+            ChatBubble(
+              message: "Do you have some knowledge about GoogleCloud",
+              isComing: false,
+              imageUrl:
+                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyDN-MudJPSedBOTxMj6q9ni7G1-o49uk8Xg&s",
+              status: "seen",
+              time: "10:01 AM",
+            ),
+            ChatBubble(
+              message: "No don't have any knowledge about it",
+              isComing: true,
+              imageUrl: "",
+              status: "seen",
+              time: "10:01 AM",
+            ),
+            ChatBubble(
+              message: "but I have some knowledge about AWS",
+              isComing: true,
+              status: "seen",
+              time: "10:01 AM",
+              imageUrl: "",
+            ),
+            ChatBubble(
+              message:
+                  "Ohk its perfect now you can easily learn about this topic",
+              isComing: false,
+              imageUrl: "",
+              status: "seen",
+              time: "10:01 AM",
+            ),
+            ChatBubble(
+              message:
+                  "Yes you are right, i serach on google about it, you can check it out",
+              isComing: true,
+              imageUrl:
+                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4IQwf20WhpY5FanP5-POvAjcAoBUIP72YnQ&s",
+              status: "seen",
+              time: "10:01 AM",
             ),
           ],
         ),
